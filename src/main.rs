127da44ad1cee1cd72f060c92e94c3d66ba273ad
Dr. Ralf Berger <ralf@paris-ag.com>
2023-08-11 18:50:53 +0200

    Learning the Bevy ECS pattern.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn hello_world() {
+    println!("Hello World!")
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hello {}!", name.0);
+    }
+}
+
 fn main() {
-    println!("Hello World!");
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
 }
