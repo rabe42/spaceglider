6d663a39e1f993f72dd0f1ba3523b21869491d39
Dr. Ralf Berger <ralf@paris-ag.com>
2023-08-11 19:22:14 +0200

    Completed the bevy tutorial.

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,24 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-fn hello_world() {
-    println!("Hello World!")
-}
-
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Name, With<Person>>)
+{
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }
     }
 }
 
@@ -26,8 +31,9 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, greet_people));
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
     }
 }
 
