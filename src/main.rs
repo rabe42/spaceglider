4f411c35f9e1d98bd634a862cb0a9e9dc9cf1e6f
Dr. Ralf Berger <ralf@paris-ag.com>
2023-08-13 11:19:54 +0200

    Windows of primary window set to Spaceglider.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ mod game;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, game::Spaceglider))
+        .add_plugins(
+            (DefaultPlugins
+                .set(WindowPlugin {
+                        primary_window: Some(Window {
+                            title: "Spaceglider".to_string(),
+                            ..Default::default()
+                        }),
+                    ..Default::default()
+                })
+            , game::Spaceglider)
+        )
         .run();
 }
