533b8ed816786a362ea377039e62564a349c22c8
Dr. Ralf Berger <ralf@paris-ag.com>
2023-08-26 17:33:28 +0200

    First text output using the Gruppo-Regular font.

--- src/game.rs
+++ src/game.rs
@@ -8,11 +8,21 @@ struct Name(String);
 
 fn setup(mut commands: Commands,
          meshes: ResMut<Assets<Mesh>>,
-         materials: ResMut<Assets<ColorMaterial>>
+         materials: ResMut<Assets<ColorMaterial>>,
+         asset_server: Res<AssetServer>,
 ) {
     // The camera for the HUD
     commands.spawn(Camera2dBundle::default());
 
+    // Creating some Lines to be displayed on the HUD
+    commands.spawn(TextBundle::from_section(
+        "Hold 'Left' or 'Right' to change the line width",
+        TextStyle {
+            font: asset_server.load("fonts/Gruppo-Regular.ttf"),
+            font_size: 24.,
+            color: Color::WHITE,
+        },
+    ));
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
@@ -46,4 +56,3 @@ impl Plugin for Spaceglider {
             .add_systems(Update, greet_people);
     }
 }
-
