12a27d8b42dcbcea8743bea80d103f63532ee30a
Dr. Ralf Berger <ralf@paris-ag.com>
2023-10-02 11:05:32 +0200

    Dynamic texts on the display, using a component.

--- src/game.rs
+++ src/game.rs
@@ -2,11 +2,38 @@ use std::f32::consts::PI;
 
 use bevy::prelude::*;
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+#[derive(Component)]
+struct ExampleDisplay;
+
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
+
+// Note: Everything, which should be displayed must be a component or a resource. 
+// The magic happens in the function parameter lists, where an arbitrary number of Queries on
+// Resources (Res or ResMut) and Components (Query) may be executed.
+
+/// This is the basic plugin for the Spaceglider game. In the first step, I try to setup the head
+/// up dsiplay (HUD).
+pub struct Spaceglider;
+
+impl Plugin for Spaceglider {
+
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            // Setting the background color
+            .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.5)))
+            // Initializes the system
+            .add_systems(Startup, setup)
+            // Start the interaction
+            .add_systems(Update, (update_hud, greet_people));
+    }
+}
 
 /// Setup the different game parts.
 fn setup(mut commands: Commands,
@@ -27,13 +54,29 @@ fn setup(mut commands: Commands,
         },
     ));
 
+    let display_text_style = TextStyle {
+        font: asset_server.load("fonts/Gruppo-Regular.ttf"),
+        font_size: 22.,
+        color: Color::BLACK,
+    };
+
+    // Create a area, where we will write dynamic text onto.
+    // Note: We spawn a tuple here!
+    commands.spawn((
+        TextBundle::from_section("", display_text_style)
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            top: Val::Px(10.),
+            right: Val::Px(20.0),
+            ..default()
+        }),
+        ExampleDisplay,
+    ));
+
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
 
 /// Just demonstrate, that we are still alive.
 fn greet_people(
@@ -51,6 +94,7 @@ fn greet_people(
 /// Update the HUD with status data from the ships model and the space environement.
 fn update_hud(
     mut gizmos: Gizmos,
+    mut display: Query<&mut Text, With<ExampleDisplay>>,
     time: Res<Time>
 ) {
     let sin = time.elapsed_seconds().sin() * 50.;
@@ -80,21 +124,10 @@ fn update_hud(
     // Arcs default amount of segments is linerarly interpolated between
     // 1 and 32, using the arc length as scalar.
     gizmos.arc_2d(Vec2::ZERO, sin / 10., PI / 2., 350., Color::ORANGE_RED);
+
+    // Write some information on the screen
+    // Create a cross hair
+    let mut display = display.single_mut();
+    display.sections[0].value = format!("Distance: {}\nObject Type: {}", time.elapsed_seconds(), "Hypo");
 }
 
-/// This is the basic plugin for the Spaceglider game. In the first step, I try to setup the head
-/// up dsiplay (HUD).
-pub struct Spaceglider;
-
-impl Plugin for Spaceglider {
-
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            // Setting the background color
-            .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.5)))
-            // Initializes the system
-            .add_systems(Startup, setup)
-            // Start the interaction
-            .add_systems(Update, (update_hud, greet_people));
-    }
-}
